7dd965e00c7b21c8e0d813e259a41138ee7d856d
Artem Revenko <artreven@gmail.com>
2019-10-23 16:53:32 +0200

    Add __version__ to nif package

---
+++ nif/__init__.py
@@ -0,0 +1,10 @@
+__all__ = (
+    '__version__',
+    'version_info'
+)
+
+from pbr.version import VersionInfo
+
+_v = VersionInfo('mock').semantic_version()
+__version__ = _v.release_string()
+version_info = _v.version_tuple()
